10233895a62488467127c6fd032ace903fcde9eb
Heriniaina <ramaheritoheriniaina@.com>
2026-05-29 15:35:57 +0300

    Feat: organization finished

--- frontend/src/app/(auth)/components/adminCard.tsx
+++ frontend/src/app/(auth)/components/adminCard.tsx
@@ -18,7 +18,7 @@ export default function AdminCard({ isActive, onToggle }: AdminCardProps) {
         e.preventDefault();
         
         if (matricule && password) {
-            router.push('/user'); 
+            router.push('/dashboard'); 
         }
     };
 
